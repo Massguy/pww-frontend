3505253c1e0fa863e50f0ba3c3d13c0f3750997e
Massguy <stefin008@hotmail.co.uk>
2020-11-24 17:01:04 +0000

    sorting ascending and descending

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,21 +1,63 @@
 import React,{useEffect,useState} from 'react';
 import axios from 'axios';
+import '../App.css'
+
 
 const Products = () => {
     const [products, setProducts] = useState([]);
+    const [orderStatus,updateOrder] = useState('desc')
+  
     useEffect(()=>{
         axios.get("https://pww-api.herokuapp.com").then((res)=>{
-            setProducts(res.data)
+            console.log('res data', res.data); 
+            let sortedProducts = sortProdcutsByPrice(res.data);
+            setProducts(sortedProducts)
         }).catch((err)=>{
             console.log(err)
         })
+       
     },[])
+  
+
+    const sortProdcutsByPrice = (products) => {
+        let sortedProducts = products.sort(function(item1, item2){
+            return orderStatus === "asc" ? item2.price.value - item1.price.value : item1.price.value - item2.price.value;
+        });
+        return sortedProducts;
+    }
+ 
+    const onChangeOrder = (event) => {
+        updateOrder(event.target.value);
+        let sortedProducts = sortProdcutsByPrice(products);
+        setProducts(sortedProducts)
+    }
+
     return(
         <>
         <h1>List of products</h1>
+        <select onChange={onChangeOrder} value={orderStatus}>
+            <option value="asc" >Ascending</option>
+            <option value="desc" >Descending</option>
+        </select>
         {products.map((product)=>(
-            <div key={product._id}>
+            <div key={product._id} className="productsContainer">
+                <div className="productName">
                 {product.name}
+                </div>
+                <div className="productDescription">
+                {product.description}
+                </div>
+                <div className="productType">
+                {product.type}
+                </div>
+                <div className="productDepartment">
+                {product.department}
+                </div>
+                {product.weight}
+                <div className="productPrice">
+                £ {product.price.value} {product.price.currency}
+                </div>
+                
             </div>
         ))}
         </>
